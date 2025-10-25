ca93550d3786b4bf8e6c225904f90f31181a8582
Srivishnu M V <srivishnum.v06@gmail.com>
2025-10-25 15:48:34 +0530

    Langchain and Gemini integration

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-import google.generativeai as genai
+from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 import os
 
+# Load API key from .env
 load_dotenv()
 api_key = os.getenv("GOOGLE_API_KEY")
 
-# Configure API key
-genai.configure(api_key=api_key)
+# Initialize LLM
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", api_key=api_key)
 
-# Pick your model
-model = genai.GenerativeModel("models/gemini-2.5-flash")
-
-# Generate text
-response = model.generate_content("What is the meaning of life?")
-print(response.text)
+# Generate a response
+response = llm.invoke("Write a short poem about AI in Shakespearean style.")
+print(response.content)
